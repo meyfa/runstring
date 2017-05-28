2a6b57480d9c6bdde1a4cb3cbd555523454d33d7
JangoBrick <mail@jangobrick.de>
2017-05-28 12:28:25 +0200

    Fix arguments not serialized

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ module.exports = function (func /*, arg1, arg2, ... */) {
     if (typeof func !== "function") {
         throw new Error("expected a function but got " + typeof func);
     }
-    var args = Array.prototype.slice.call(func, 1);
+    var args = Array.prototype.slice.call(arguments, 1);
     return ";(" + func.toString() + ")(" + serializeArguments(args) + ");";
 };
 
 function serializeArguments(args) {
     return args.map(function (arg) {
-        return JSON.serialize(arg);
+        return JSON.stringify(arg);
     }).join(", ");
 }
